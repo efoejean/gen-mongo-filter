f14c893fae501ca2a71054e74a80c1316e7c4b66
Manav Misra <manavm@visionwebsoft.com>
2021-11-18 21:50:55 -0600

    Fix girections to be a proper Mongo filter

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ app.get("/api/listings", (req, res) => {
    * TODO: Use Insomnia to test this endpoint:
    * 'http://localhost:3000/api/listings?price-lte=250&price-gte=150&beds=2&bed_type=real+bed'
    *
-   * TODO: Use `reduce` to generate: `{ "price-lte": 250, "price-gte": 150, "beds": 2, "bed_type": "real bed" }`. Hint: `Object.entries`.
+   * TODO: Use `reduce` to generate: `{ price: {$lte: 250}, price: {$gte: 150}, beds: 2, bed_type:"Real Bed" }`. Hint: `Object.entries`.
    *
    * TODO: Send the response back as JSON. Hint: `res.json`.
    */
