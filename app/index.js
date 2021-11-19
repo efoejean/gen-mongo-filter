5d9f4ea2b2d970e13957ecb93b0555e539588bd9
Manav Misra <manavm@visionwebsoft.com>
2021-11-18 21:21:19 -0600

    :seedling:

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+
+const app = express();
+
+app.get("/api/listings", (req, res) => {
+  /**
+   * TODO: Use Insomnia to test this endpoint:
+   * 'http://localhost:3000/api/listings?price-lte=250&price-gte=150&beds=2&bed_type=real+bed'
+   *
+   * TODO: Use `reduce` to generate: `{ "price-lte": 250, "price-gte": 150, "beds": 2, "bed_type": "real bed" }`. Hint: `Object.entries`.
+   *
+   * TODO: Send the response back as JSON. Hint: `res.json`.
+   */
+});
